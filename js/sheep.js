4401517d789fdcb61e2f4f4bdee10f10e400563a
Will <will@mindfuzz.net>
2013-03-08 01:50:24 +0000

    Working fractal.
    Need to add more palettes, more functions, and render progress.

--- js/sheep.js
+++ js/sheep.js
@@ -6,108 +6,160 @@
  * a = [global] canvas 2d context
  * b = [global] document.body
  * c = [global] canvas element
- *
+ * d = largest hit per pixel
+ * e = colour coordinate
  * f = function chosen randomly
- * g = colour to plot
- *
+ * g = m[f]
  * h = [global] canvas height
  * i = iterator for loop
- *
+ * j = iterator for loop
+ * k = z.data floats
+ * l = index of pixal in image buffer
  * m = [global] matrix of coefficients
- * p = [global] array of probabilities
- *
- * q = function argument
- * r = function argument, random number
- * s = function argument
- * t = function argument, temp variable
- * u = function argument
- * v = function argument
- *
+ * n = 
+ * o = [global] array of functions
+ * p = 
+ * q = 
+ * r = x*x + y*y
+ * s = scale value
+ * t = temp variable
+ * u = scaled x coordinate
+ * v = scaled y coordinate
  * w = [global] canvas width
- *
  * x = x coordinate
  * y = y coordinate
  * z = [global] image buffer
  */ 
 
-// setup canvas and window
-w = c.width = 300;      //window.innerWidth;
-h = c.height = 300;     //window.innerHeight;
-
-// create image buffer
-// everything (r,g,b,a) will be zero
-z = a.createImageData(w,h);
-
-// Barnsley fern
+// index of functions, and aliases
 /*
-p = [0.01, 0.07, 0.07, 0.85];
-// function coefficients
+ * F = render a flame
+ * R = random number [0-1]
+ * M = Math
+ * W = window
+ */
+
+// W=window;
+M=Math;
+W=window;
+
+// setup canvas and window
+w = c.width = W.innerWidth;
+h = c.height = W.innerHeight;
+
+// m = [
+//     [0.562482, -0.539599, 0.397861, 0.501088, -0.42992, -0.112404],
+//     [0.830039, 0.16248, -0.496174, 0.750468, 0.91022, 0.288389]
+// ];
+
 m = [
-    [0,     0,     0,     0.16, 0, 0   ], // function 1 (prob, p[0])
-    [0.2,   -0.26, 0.23,  0.22, 0, 1.6 ], // function 2 (prob, p[1])
-    [-0.15, 0.28,  0.26,  0.24, 0, 0.44], // function 3 (prob, p[2])
-    [0.85,  0.04,  -0.04, 0.85, 0, 1.6 ]  // function 4 (prob, p[3])
-];
-*/
-
-// Some other example from a PDF
-p = [0.5, 0.5];
-m = [
-    [ 0.164856, -0.775017, 0.664133, 0.504859, -0.400526, 0.155692 ],
-    [ -0.14321, -0.540632, 0.427958, 0.0901299, 2.14451, 2.54161 ]
+    [-R(), R(), -R(), R(), -R(), R()],
+    [R(), -R(), R(), -R(), R(), -R()]
 ];
 
-// render
-function render(q, r, s, t, u, v) {
+// colour palette
+// 0: reddish
+n = [
+    [
+        [0,  '[t*2.5,0,0]'],
+        [.25,'[1,(t-.25)*2.5,0]'],
+        [.50,'[1-((t-.5)*.4),1,0]'],
+        [.75,'[1,1,(t-.75)*2]']
+    ]
+];
 
-    // clear screen
-    a.fillStyle = '#000';
-    a.fillRect(0, 0, w, h);
+/*
+ * Return a random number
+ */
+function R() { 
+    return M.random();
+}
 
-    // initial conditions
+/*
+ * Render a flame to canvas
+ */
+function F() {
+
+    // create image buffer
+    z = a.createImageData(w,h);
+
+    // random x,y in -1,1 plane
+    // OPT: still works at (0,0)
     x = 0;
     y = 0;
 
-    for (var i = 0; i < 80000; i++) {
+    // track largest pixel hit
+    d = 0;
 
-        // random number
-        r = Math.random();
+    // a while loop is smaller than a for loop
+    i = 0;
+    while (i < 10000000) {
 
-        // pick f randomly
-        f = 0; // function to choose
-        g = 0; // running total of probability
+        // pic a function randomly
+        g = m[M.floor(R()*m.length)];
 
-        for (j in p) {
-            g += p[j];
-            if (r <= g) break;
-            f++;
-        }
-
-        t = m[f][0] * x + m[f][1] * y + m[f][4];
-        y = m[f][2] * x + m[f][3] * y + m[f][5];
+        // linear equations
+        t = g[0] * x + g[1] * y + g[4];
+        y = g[2] * x + g[3] * y + g[5];
         x = t;
 
-        plot(x, y, '#fff');
+        // variables for variations
+        r = (x*x + y*y);
+
+        // spherical
+        x = x/(r);
+        y = y/(r);
+
+        // scale x and y coordinates to fit in z
+        u = M.round(x*(w/2) + (w/2), 0);
+        v = M.round(y*(h/2) + (h/2), 0);
+
+        // calculate location of pixel in image buffer
+        l = 4*(u*w + v);
+
+        // only if it's actually inside the array...
+        if (0 <= l < 4*w*h) {
+            // add hit to alpha
+            z.data[l+3] += 1;
+
+            // keep largest hit
+            if (z.data[l+3] > d)
+                d = z.data[l+3];
+        }
+
+        // increase i
+        i++;
     }
 
+    // prepare image buffer
+    i = 0;
+    while ( i < 4*w*h ) {
+        // logarithmic scale
+        t = (M.log(z.data[i+3])/M.log(d));
+
+        if (t > 0) {
+
+            for (j in n[0])
+                if (t > n[0][j][0])
+                    q = eval(n[0][j][1]);
+
+            // set nicely!
+            z.data[i+0] = 255*q[0];
+            z.data[i+1] = 255*q[1];
+            z.data[i+2] = 255*q[2];
+
+        }
+
+        // set alpha to full
+        z.data[i+3] = 255;
+
+        i+=4;
+    }
+    
+    // copy buffer to canvas
+    a.putImageData(z, 0, 0);
 }
 
-/*
- * q - x coord
- * r - y coord
- * s - colour
- */
-function plot(q, r, s, t, u, v) {
-    // map xy plane to canvas
-    q *= 30;
-    r *= 30;
+// render flame
+F();
 
-    q += w/2;
-    r += h/3;
-
-    // plot pixel of correct colour
-    a.fillStyle = s;
-    a.fillRect(q, r, 0.5, 0.5);
-}
-
-render();
